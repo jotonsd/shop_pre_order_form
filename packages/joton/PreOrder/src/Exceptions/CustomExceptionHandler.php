2a0958c5e26b81d60cb513de89ca27ce5af7f70d
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2024-11-23 23:54:23 +0600

    :up: Update: Re-format error handler

--- packages/joton/PreOrder/src/Exceptions/CustomExceptionHandler.php
+++ packages/joton/PreOrder/src/Exceptions/CustomExceptionHandler.php
@@ -2,26 +2,67 @@
 
 namespace Joton\PreOrder\Exceptions;
 
-use Exception;
 use Throwable;
-use Joton\PreOrder\Traits\LoggerTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class CustomExceptionHandler extends ExceptionHandler
 {
-    use LoggerTrait;
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     */
+    public function report(Throwable $exception): void
+    {
+        parent::report($exception);
+    }
+
     /**
      * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Illuminate\Http\Response
      */
     public function render($request, Throwable $exception)
     {
+        // Handle ValidationException
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation Error',
+                'errors' => $exception->validator->errors()->messages(),
+            ], 422);
+        }
+
+        // Handle HTTP exceptions
+        if ($exception instanceof HttpException) {
+            return response()->json([
+                'success' => false,
+                'message' => $exception->getMessage() ?: 'HTTP Exception',
+            ], $exception->getStatusCode());
+        }
+
+        // Default exception response format
         return response()->json([
             'success' => false,
-            'message' => $exception->getMessage(),
-        ], 400);
+            'message' => $exception->getMessage() ?: 'Server Error',
+            'trace' => config('app.debug') ? $exception->getTrace() : null,
+        ], 500);
     }
 }
