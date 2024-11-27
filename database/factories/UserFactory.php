8e90d1a2e5e0aefe75e7dd4d0029546a1bc97df3
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2024-11-27 22:08:07 +0600

    :up: Update: user factory for creating admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => "Admin User",
+            'email' => "admin@gmail.com",
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'role' => 'admin',
+            'password' => Hash::make('12334567'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
